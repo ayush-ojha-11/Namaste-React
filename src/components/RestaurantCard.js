dbbd986f466fdde9ea51b0028a4c8970dc8aac12
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-01-23 12:27:26 +0530

    Changes in css and others

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -9,11 +9,13 @@ const RestaurantCard = (props) => {
         className="res-logo"
         src={CDN_URL + resData.info.cloudinaryImageId}
       />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{resData.info.sla.deliveryTime + " minutes"}</h4>
+      <div className="res-card-info-container">
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{resData.info.sla.deliveryTime + " minutes"}</h4>
+      </div>
     </div>
   );
 };
