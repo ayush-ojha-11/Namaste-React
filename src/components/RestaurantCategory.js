e10baa38551f8693b4f5b85e56247dd745ec395b
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-02-03 23:01:52 +0530

    Built Acciordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
-  const [showItems, setShowItems] = useState(false);
-  const handleClickOnCategory = () => {};
+  const [showItems, setShowItems] = useState(true);
+  const handleClickOnCategory = () => {
+    setShowItems(!showItems);
+  };
   return (
     <div className="lg:w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4">
-      <div className=" flex justify-between" onClick={handleClickOnCategory}>
+      <div
+        className=" flex justify-between cursor-pointer"
+        onClick={handleClickOnCategory}
+      >
         <span className="text-md font-bold">
           {data.title} ({data.itemCards.length})
         </span>
