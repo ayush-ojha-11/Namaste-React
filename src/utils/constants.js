3e328e94095fc0d424503bdb622030b3fe6f2c2c
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-02-06 02:59:44 +0530

    revert

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const MENU_URL =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+  "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
